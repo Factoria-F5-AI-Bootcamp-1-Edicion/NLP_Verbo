b016eaeb0cd7f9ba88142c354d798938f86b6fa5
AnadeCordoba <ana.decordoba@gmail.com>
2022-12-09 11:23:56 +0100

    segunda version

--- nlp-front.py
+++ nlp-front.py
@@ -42,7 +42,7 @@ lottie_url_hello = "https://assets7.lottiefiles.com/packages/lf20_3vbOcw.json"
 lottie_url_download = "https://assets10.lottiefiles.com/packages/lf20_q56zavhf.json"
 lottie_url_transition1= "https://assets10.lottiefiles.com/temp/lf20_tXDjQg.json"
 lottie_url_home = "https://assets10.lottiefiles.com/packages/lf20_xgdvjjxc.json"
-lottie_url_predict = "https://assets8.lottiefiles.com/temp/lf20_jbSzVz.json"
+lottie_url_predict = "https://assets7.lottiefiles.com/packages/lf20_feesoomz.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 lottie_home = load_lottieurl(lottie_url_home)
 lottie_download = load_lottieurl(lottie_url_download)
@@ -103,19 +103,22 @@ selected = streamlit_menu(example=EXAMPLE_NO)
 
 
 if selected == "Home":
+
+    st.markdown("<h1 style='text-align: center; color: purple;'>DeepIA Tech</h1>", unsafe_allow_html=True)
     
-    st.title("Welcome to DeepIA Text")
-    st.image("2.png")
-    # 
-    st.markdown("<h3 style='text-align: justify; color: black;'>EDNA is an artificial intelligence that can predict if a person is susceptible to suffer an stroke. You can make EDNA give a personalize prediction by filling the form a pressing a button.             Is that Simple!</h3>", unsafe_allow_html=True)
+
+    st.image("Healthy.png")
     
+    st.markdown("<h5 style='text-align: justify; color: black;'>DeepIA Tech es una consultoría en la que nos encargamos de buscar soluciones tecnológicas. Nuestro equipo está conformado por data analystics, desarrolladores y desarrolladoras de Inteligencia Artificial, especialistas en marketing y diseño web.</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: justify; color: black;'>Así nace Healthy Comments es una aplicación para el análisis de comentarios, como en las redes sociales, que nos ayuda a identificar si son tóxicos o no, utilizando Machine Learning. Queremos seguir desarrollando nuestra herramienta  para que sea mas polivalente e incluso llegar a poder restringir de manera automática los comentarios nocivos hacia otras personas.</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: justify; color: black;'>Contamos con soporte técnico que ayuda a mantener la herramienta de forma constante y ofrecemos capacitación a las empresas.</h5>", unsafe_allow_html=True)
 
 
 
 if selected== "Prediction":
     margin()
+    st.markdown("<h1 style='text-align: center; color: purple;'>Ingresa el comentario a clasificar en la cajita de abajo</h1>", unsafe_allow_html=True)
 
-    st.title("Hello!, please write a comment to make a prediction!")
     st_lottie(lottie_predict, key="predict",
         speed=1,
         reverse=False,
@@ -128,7 +131,7 @@ if selected== "Prediction":
 
 
 
-    txt = st.text_area('Text to analyze', '''
+    txt = st.text_area('Aqui va tu comentario para clasificar', '''
     It was the best of times, it was the worst of times, it was
     the age of wisdom, it was the age of foolishness, it was
     the epoch of belief, it was the epoch of incredulity, it
